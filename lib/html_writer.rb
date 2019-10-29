6a9a81b0e2e80fcedc2720edf6a88c5c4ca4a1ed
Vaqim <vadimser1355@gmail.com>
2019-10-29 10:15:05 +0200

    valid html

--- lib/html_writer.rb
+++ lib/html_writer.rb
@@ -14,7 +14,19 @@ class HtmlWriter
 
   def self.write(content, filename: 'index', bypass_html: false)
     content = ::Sanitize.clean(content) unless bypass_html
-    content = "<html><head/><body>#{content}</body></html>"
+    content = "
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Pet stats</title>
+        <link rel="stylesheet" href="style.css">
+      </head>
+      <body>
+        #{content}
+      </body>
+    </html>
+    "
     raise InvalidHTMLError, 'Content isn\'t valid html' if Nokogiri::HTML(content, &:strict).errors.any?
 
     File.write("#{filename}.html", content)
